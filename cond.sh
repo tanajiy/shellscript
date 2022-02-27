d12b78c3c4e8a00dcfe23f9911e52c6ca696c6bf
Tanaji <tanajiyadav1@gmail.com>
2022-02-27 12:45:51 +0530

    update

--- cond.sh
+++ cond.sh
@@ -5,4 +5,12 @@ read num1
 if [ $num1 -lt 10 ]
 then
 echo "This is single digit number"
+elif [[ $num1 -gt 9 && $num1 -lt 100]]
+then 
+echo "This is double digit number"
+elif [[ $num1 -gt 99 && $num1 -lt 1000]]
+then
+echo "This is third digit number"
+else 
+echo "This number is more than 3 digit"
 fi
